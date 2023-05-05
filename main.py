e6a670fe4fcb644fc0bfcf58305ed65f632ba923
Octavio <ferreyraoctavio6@gmail.com>
2023-05-05 20:04:45 -0300

    creation of the bot

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import os, json
 import discord
-import os
+from dotenv import load_dotenv
+from discord.ext import commands
+def main():
+    load_dotenv()
+    prefix = "!"
+    token = os.getenv("DISCORD_TOKEN")
+    intents = discord.Intents.all()
+    bot = commands.Bot(command_prefix=prefix, intents=intents, description="hola, soy el Dolar Bot")
 
-bot = discord.Client()
+    # commands and events
+    @bot.command(name="saludar",help="el bot te saludara")
+    async def saludar(ctx):
+        await ctx.reply(f"hola {ctx.author}, soy el DolarBot")
+    
+    bot.run(token)
 
-@bot.event
-async def on_ready():
-    guild_count = 0
+if __name__ == "__main__":
+    main()
